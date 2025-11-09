d4f89147b700cfa33617fc833f6e627a71ac78c2
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2025-11-08 19:44:56 -0500

    Update main.rs
    This update fixes some bugs and makes the code slightly shorter.

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,18 @@ fn main() {
 
     args.remove(0);
 
-    let pos_help = args.iter().position(|n| n == "--help");
-    let pos_version = args.iter().position(|n| n == "--version");
+    let pos_help = args.iter().position(|n| n == "--help").unwrap_or(usize::MAX);
+    let pos_version = args.iter().position(|n| n == "--version").unwrap_or(usize::MAX);
 
-    // Begin compatibility with GNU extensions
     if pos_help < pos_version {
-        help();
+        println!("Outputs the string provided in the arguments forever. If no string is given, outputs 'y' repeatedly.");
+        println!("For version information, invoke with --version.");
         return;
     }
 
     if pos_help > pos_version {
-        version();
+        println!("yes (Mojavesoft implementation); should be compatible with GNU yes v9.5");
+        println!("Copyright November 2025.");
         return;
     }
 
@@ -25,18 +26,7 @@ fn main() {
         b"y\n"
     } else {joined = args.join(" ") + "\n"; joined.as_bytes()};
 
-    // End compatibility with GNU extensions
     let mut io_handle = io::stdout().lock();
     let buf = output_str.repeat(4096);
     loop {if io_handle.write_all(&buf).is_err() {return;}}
-}
-
-fn help() {
-    println!("Outputs the string provided in the arguments forever. If no string is given, outputs 'y' repeatedly.");
-    println!("For version information, invoke with --version.");
-}
-
-fn version() {
-    println!("yes (Mojavesoft implementation); should be compatible with GNU yes v9.5");
-    println!("Copyright November 2025.");
 }
